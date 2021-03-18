a58c818ed409fb592a3c9f1466075cb1ce8f855f
rodriguezny <rodrigueznyiringabo@gmail.com>
2021-03-18 23:18:17 +0100

    ft-component content contextualisation

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App extends Component {
   }
 
   render() {
+    const won = new Date().getSeconds() % 2 === 0
     return (
       <div className="memory">
         <GuessCount guesses={0} />
@@ -18,6 +19,7 @@ class App extends Component {
         <Card card="🎩" feedback="visible" onClick={this.handleCardClick} />
         <Card card="🐶" feedback="hidden" onClick={this.handleCardClick} />
         <Card card="🐱" feedback="justMatched" onClick={this.handleCardClick} />
+        {won && <p>Gagné</p>}
       </div>
     )
   }

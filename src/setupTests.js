1ac2924a8de510393d5fa7303fdb855a00b5388d
rodriguezny <rodrigueznyiringabo@gmail.com>
2021-04-08 10:16:57 +0100

    ft-setup test implementation

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,8 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import chai from 'chai'
+import dirtyChai from 'dirty-chai'
+import createChaiJestDiff from 'chai-jest-diff'
+
+chai.use(dirtyChai).use(createChaiJestDiff())
